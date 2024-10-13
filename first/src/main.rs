f6d8ed8c3cf2adf009b34295288ee5f5014d3514
Prakhar Mishra <prakhar.mishra1990@gmail.com>
2024-10-13 09:44:55 +0000

    first test project, exploring vars and funcs

---
+++ first/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let hello_world_msg = "Hello World!";
+    let name = "Prakhar Mishra";
+    let age = 34;
+    let salary = 1234.56;
+
+    println!("{} {}", hello_world_msg, get_message(name, age, salary));
+}
+
+fn get_message(name: &str, age: i32, salary: f64) -> String {
+    format!("My name is {}, age {}. I am earning {} per month.", name, age, salary)
+}
