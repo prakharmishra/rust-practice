78a55a8a99e40e921a66b3b999ec7135bbf0b0f8
Prakhar Mishra <prakhar.mishra1990@gmail.com>
2024-10-14 18:47:43 +0000

    Tried using if expression

--- second/src/main.rs
+++ second/src/main.rs
@@ -1,10 +1,19 @@
 fn main() {
     let x: Option<String> = Some(String::from("testing"));
-    if let Some(y) = x {
+    if let Some(y) = &x {
         println!("Valid value: {}", y);
     } else {
         println!("x is not there");
     }
+
+    let x = None;
+    let val = if x.is_some() {
+        x.unwrap()
+    } else {
+        340.0
+    };
+
+    println!("{val}");
 
     // println!("Hello, world! {}", x);
 }
